ec7d3b13301f28b542ddef496861a02e2b354373
Lars Malach <lam@koempf.de>
2017-03-16 10:32:45 +0100

    TASK: Replace typo3 with new neos namespace

--- Classes/Ttree/Scheduler/Annotations/Meta.php
+++ Classes/Ttree/Scheduler/Annotations/Meta.php
@@ -2,14 +2,14 @@
 namespace Ttree\Scheduler\Annotations;
 
 /*                                                                        *
- * This script belongs to the TYPO3 Flow package "Ttree.Scheduler".       *
+ * This script belongs to the Neos Flow package "Ttree.Scheduler".       *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU General Public License, either version 3 of the   *
  * License, or (at your option) any later version.                        *
  *                                                                        */
 
-use TYPO3\Flow\Annotations as Flow;
+use Neos\Flow\Annotations as Flow;
 
 /**
  * @Annotation
